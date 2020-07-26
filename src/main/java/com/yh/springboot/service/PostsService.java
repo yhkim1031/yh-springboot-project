620fe2e39407ce11c5210a95f3a178b21b62b9d7
Younghoon Kim <yhkim1031@gmail.com>
2020-07-26 22:14:21 +0900

    PostsService added

---
+++ src/main/java/com/yh/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.yh.springboot.service;
+
+import com.yh.springboot.domain.posts.PostsRepository;
+import com.yh.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
