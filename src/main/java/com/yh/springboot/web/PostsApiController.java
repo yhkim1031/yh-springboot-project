820581a3eeb0813745b17b5d26970ad92e3416a9
Younghoon Kim <yhkim1031@gmail.com>
2020-07-27 21:46:26 +0900

    PostsService Put and GetMapping update

--- src/main/java/com/yh/springboot/web/PostsApiController.java
+++ src/main/java/com/yh/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.yh.springboot.web;
 
 import com.yh.springboot.service.PostsService;
+import com.yh.springboot.web.dto.PostsResponseDto;
 import com.yh.springboot.web.dto.PostsSaveRequestDto;
+import com.yh.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,4 +17,15 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
 }
