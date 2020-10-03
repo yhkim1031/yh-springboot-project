f934aa297b90e20b1731fe19feb47db573653209
Younghoon Kim <yhkim1031@gmail.com>
2020-10-03 16:13:55 +0900

    OAuthAttributes naver login fix

--- src/main/java/com/yh/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/yh/springboot/config/auth/dto/OAuthAttributes.java
@@ -46,9 +46,9 @@ public class OAuthAttributes {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
         return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
-                .picture((String) attributes.get("picture"))
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
                 .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
