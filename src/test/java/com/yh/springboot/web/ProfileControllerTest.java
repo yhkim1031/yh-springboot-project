89f1cb35e4121371884e08dda3ce01ae82812e8d
Younghoon Kim <yhkim1031@gmail.com>
2020-10-03 21:00:47 +0900

    Profile 기능 추가

---
+++ src/test/java/com/yh/springboot/web/ProfileControllerTest.java
@@ -0,0 +1,29 @@
+package com.yh.springboot.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerTest {
+
+    @Test
+    public void real_profile이_조회된다(){
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+
+}
